b0926c7b5a9d2b39358d43c0c9fbb78a6af79dfe
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-23 20:24:22 +0900

    ADD: rubocop 追加

--- app/controllers/google_calender_api_controller.rb
+++ app/controllers/google_calender_api_controller.rb
@@ -1,5 +1,9 @@
 class GoogleCalenderApiController < ApplicationController
   def index
+    scope = "https://www.googleapis.com/auth/calendar"
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+                json_key_io: File.open(Rails.root.join('app/controllers/google_calender_api_controller.rb').to_s),
+                scope: scope)
 
   end
 end
