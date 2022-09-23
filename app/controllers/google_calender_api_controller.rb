c893bd5da4009307fcf9e570cd62fb2dff6ebd85
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-23 21:48:33 +0900

    ADD: サービスアカウントでカレンダー情報取得

--- app/controllers/google_calender_api_controller.rb
+++ app/controllers/google_calender_api_controller.rb
@@ -1,9 +1,15 @@
 class GoogleCalenderApiController < ApplicationController
   def index
-    scope = "https://www.googleapis.com/auth/calendar"
-    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
-                json_key_io: File.open(Rails.root.join('app/controllers/google_calender_api_controller.rb').to_s),
-                scope: scope)
-
+    calendar = Google::Apis::CalendarV3::CalendarService.new
+    calendar.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open('/Users/ryotaro/Desktop/program_study/rails/google_calender/calender-project-358005-91cafccfb196.json'),
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR
+    )
+    calendar.authorization.fetch_access_token!
+    # entry = Google::Apis::CalendarV3::CalendarListEntry.new(
+    # id: 'calendarId'
+    # )
+    # byebug
+    calendar.list_events("ryotaro123110@gmail.com").items[-1]
   end
 end
