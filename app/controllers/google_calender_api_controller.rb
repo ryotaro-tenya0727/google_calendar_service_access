576b0ae923626a9362879d9869edc0d0a52691cf
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-23 20:07:01 +0900

    ADD: API追加

---
+++ app/controllers/google_calender_api_controller.rb
@@ -0,0 +1,5 @@
+class GoogleCalenderApiController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get  '/', to: 'static#index'
+  resources :google_calender_api
 end
